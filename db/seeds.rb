126e9dfa76219a86f27efb94ff165fe7da29bb96
kiboma <omayioben@gmail.com>
2022-11-23 10:58:18 +0300

    Update database with data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Book.create!(title: "My first book", body: "Hello world 1")
+Book.create!(title: "My second book", body: "Hello world 2")
+Book.create!(title: "My third book", body: "Hello world 3")
+Book.create!(title: "My forth book", body: "Hello world 4")
+Book.create!(title: "My fifth book", body: "Hello world 5")
